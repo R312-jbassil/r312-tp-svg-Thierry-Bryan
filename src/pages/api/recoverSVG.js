702006a992da46ef01f6b487a3eb2b9b001f62e5
Thierry-Bryan <bryan.thierry@edu.univ-fcomte.fr>
2025-09-30 15:11:44 +0200

    Création gallery SVG

--- src/pages/api/recoverSVG.js
+++ src/pages/api/recoverSVG.js
@@ -1,20 +1,25 @@
-import pb from "../../utils/pb";
+import pb from "../../utils/pb.js";
+import { Collections } from "../../utils/pocketbase-types.js";
 
 export const GET = async () => {
   try {
+    console.log("Récupération des SVG...");
+
     // Récupère tous les SVG
-    const records = await pb.collection("librairie").getFullList({
+    const records = await pb.collection(Collections.Svg).getFullList({
       sort: "-created",
     });
+
+    console.log("SVG trouvés:", records);
 
     return new Response(JSON.stringify({ svgs: records }), {
       headers: { "Content-Type": "application/json" },
     });
   } catch (error) {
-    console.error("Erreur:", error);
-    return new Response(JSON.stringify({ error: error.message }), {
-      status: 500,
+    console.error("Erreur lors de la récupération:", error);
+    return new Response(JSON.stringify({ svgs: [], error: error.message }), {
       headers: { "Content-Type": "application/json" },
+      status: 500,
     });
   }
 };
